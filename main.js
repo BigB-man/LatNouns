f030b0232b724dc74137dbd830e26c6c092d2ab8
BigB-man <93968558+BigB-man@users.noreply.github.com>
2023-04-23 12:01:34 +1000

    elec-storage-test
    Start of testing electron-json-storage, temporary html elements to test the storage: an input and two buttons

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log(`Hello from Electron 👋`)
+var text = "";
+
+const { app, BrowserWindow } = require('electron');
+const storage = require('electron-json-storage');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+})
+
+function addTo(){
+  text = document.getElementById("typeMe").value;
+}
