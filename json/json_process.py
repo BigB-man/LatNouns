34d1383ef50a9321fa7d3d88b5d31214ee6560d5
BigB-man <93968558+BigB-man@users.noreply.github.com>
2023-12-01 21:39:38 +1100

    python working prototype

--- json/json_process.py
+++ json/json_process.py
@@ -7,32 +7,35 @@ script_directory = os.path.dirname(os.path.realpath(__file__))
 # Set the working directory to the script's directory
 os.chdir(script_directory)
 
-sourcefile = 'Is_A.json'
+sourcefile = 'ls_A.json'
 #sourcefile = input()
 filename = 'NounDeclension1.json'
 print("Current working directory:", os.getcwd())
 
-with open(sourcefile, 'r') as k:
-    source = json.load(k)
+with open(sourcefile, 'r', encoding='utf-8') as f:
+    source = json.load(f)
 
-with open(filename, 'r') as f:
-    data = json.load(f)
+with open(filename, 'r') as k:
+    data = json.load(k)
     data['id'] = 134 # <--- add `id` value.
 
-for i in source:
+for i in source:#declension 1, i still need to implement to change for masculine words
     if i["entry_type"] == "main":
         if i["part_of_speech"] == "noun":
             if i["declension"] == 1:
-                data[i["key"]]["nom"]["sing"] = i["key"][:-1] + data["FORM"]["nom"]["sing"]
-                data[i["key"]]["nom"]["plur"] = i["key"][:-1] + data["FORM"]["nom"]["plur"]
-                data[i["key"]]["acc"]["sing"] = i["key"][:-1] + data["FORM"]["acc"]["sing"]
-                data[i["key"]]["acc"]["plur"] = i["key"][:-1] + data["FORM"]["acc"]["plur"]
-                data[i["key"]]["gen"]["sing"] = i["key"][:-1] + data["FORM"]["gen"]["sing"]
-                data[i["key"]]["gen"]["plur"] = i["key"][:-1] + data["FORM"]["gen"]["plur"]
-                data[i["key"]]["dat"]["sing"] = i["key"][:-1] + data["FORM"]["dat"]["sing"]
-                data[i["key"]]["dat"]["plur"] = i["key"][:-1] + data["FORM"]["dat"]["plur"]
-                data[i["key"]]["abl"]["sing"] = i["key"][:-1] + data["FORM"]["abl"]["sing"]
-                data[i["key"]]["abl"]["plur"] = i["key"][:-1] + data["FORM"]["abl"]["plur"]
+                if i["gender"] == "F":
+                    data.update({i["key"]:{"nom":{"sing":i["key"][:-1] + data["FORM"]["nom"]["sing"], "plur":i["key"][:-1] + data["FORM"]["nom"]["plur"]},
+                                            "acc":{"sing":i["key"][:-1] + data["FORM"]["acc"]["sing"], "plur":i["key"][:-1] + data["FORM"]["acc"]["plur"]},
+                                            "gen":{"sing":i["key"][:-1] + data["FORM"]["gen"]["sing"], "plur":i["key"][:-1] + data["FORM"]["gen"]["plur"]},
+                                            "dat":{"sing":i["key"][:-1] + data["FORM"]["dat"]["sing"], "plur":i["key"][:-1] + data["FORM"]["dat"]["plur"]},
+                                            "abl":{"sing":i["key"][:-1] + data["FORM"]["abl"]["sing"], "plur":i["key"][:-1] + data["FORM"]["abl"]["plur"]},
+                                            "Definition":i["senses"]}})
+                # data[i["key"]]["gen"]["sing"] = i["key"][:-1] + data["FORM"]["gen"]["sing"]
+                # data[i["key"]]["gen"]["plur"] = i["key"][:-1] + data["FORM"]["gen"]["plur"]
+                # data[i["key"]]["dat"]["sing"] = i["key"][:-1] + data["FORM"]["dat"]["sing"]
+                # data[i["key"]]["dat"]["plur"] = i["key"][:-1] + data["FORM"]["dat"]["plur"]
+                # data[i["key"]]["abl"]["sing"] = i["key"][:-1] + data["FORM"]["abl"]["sing"]
+                # data[i["key"]]["abl"]["plur"] = i["key"][:-1] + data["FORM"]["abl"]["plur"]
 
 os.remove(filename)
 with open(filename, 'w') as f:
