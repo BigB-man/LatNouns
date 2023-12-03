e3f5b9da88dd5f46c043c0c38fef5511a6093bcd
BigB-man <93968558+BigB-man@users.noreply.github.com>
2023-12-03 22:58:00 +1100

    Hi
    swag

--- json/json_process.py
+++ json/json_process.py
@@ -6,37 +6,41 @@ script_directory = os.path.dirname(os.path.realpath(__file__))
 
 # Set the working directory to the script's directory
 os.chdir(script_directory)
-
-sourcefile = 'ls_A.json'
-#sourcefile = input()
 filename = 'NounDeclension1.json'
 print("Current working directory:", os.getcwd())
-
-with open(sourcefile, 'r', encoding='utf-8') as f:
-    source = json.load(f)
-
+letters =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Z']
 with open(filename, 'r') as k:
     data = json.load(k)
-    data['id'] = 134 # <--- add `id` value.
-
-for i in source:#declension 1, i still need to implement to change for masculine words
-    if i["entry_type"] == "main":
-        if i["part_of_speech"] == "noun":
-            if i["declension"] == 1:
-                if i["gender"] == "F":
-                    data.update({i["key"]:{"nom":{"sing":i["key"][:-1] + data["FORM"]["nom"]["sing"], "plur":i["key"][:-1] + data["FORM"]["nom"]["plur"]},
-                                            "acc":{"sing":i["key"][:-1] + data["FORM"]["acc"]["sing"], "plur":i["key"][:-1] + data["FORM"]["acc"]["plur"]},
-                                            "gen":{"sing":i["key"][:-1] + data["FORM"]["gen"]["sing"], "plur":i["key"][:-1] + data["FORM"]["gen"]["plur"]},
-                                            "dat":{"sing":i["key"][:-1] + data["FORM"]["dat"]["sing"], "plur":i["key"][:-1] + data["FORM"]["dat"]["plur"]},
-                                            "abl":{"sing":i["key"][:-1] + data["FORM"]["abl"]["sing"], "plur":i["key"][:-1] + data["FORM"]["abl"]["plur"]}}})
-                # data[i["key"]]["gen"]["sing"] = i["key"][:-1] + data["FORM"]["gen"]["sing"]
-                # data[i["key"]]["gen"]["plur"] = i["key"][:-1] + data["FORM"]["gen"]["plur"]
-                # data[i["key"]]["dat"]["sing"] = i["key"][:-1] + data["FORM"]["dat"]["sing"]
-                # data[i["key"]]["dat"]["plur"] = i["key"][:-1] + data["FORM"]["dat"]["plur"]
-                # data[i["key"]]["abl"]["sing"] = i["key"][:-1] + data["FORM"]["abl"]["sing"]
-                # data[i["key"]]["abl"]["plur"] = i["key"][:-1] + data["FORM"]["abl"]["plur"]
+index = 0
+for j in letters:
+    sourcefile = 'ls_'+letters[index]+'.json'
+    with open(sourcefile, 'r', encoding='utf-8') as f:
+        source = json.load(f)
+        index +=1
+    for i in source:#declension 1, i still need to implement to change for masculine words
+        if i["entry_type"] == "main":
+            if i["part_of_speech"] == "noun":
+                if i["declension"] == 1:
+                    if i["gender"] == "F":
+                        data["Fem"].update({i["key"]:{
+                            "nom":{
+                                "sing":i["key"][:-1] + data["Fem"]["FORM"]["nom"]["sing"], 
+                                "plur":i["key"][:-1] + data["Fem"]["FORM"]["nom"]["plur"]},
+                            "acc":{
+                                "sing":i["key"][:-1] + data["Fem"]["FORM"]["acc"]["sing"], 
+                                "plur":i["key"][:-1] + data["Fem"]["FORM"]["acc"]["plur"]},
+                            "gen":{
+                                "sing":i["key"][:-1] + data["Fem"]["FORM"]["gen"]["sing"], 
+                                "plur":i["key"][:-1] + data["Fem"]["FORM"]["gen"]["plur"]},
+                            "dat":{
+                                "sing":i["key"][:-1] + data["Fem"]["FORM"]["dat"]["sing"], 
+                                "plur":i["key"][:-1] + data["Fem"]["FORM"]["dat"]["plur"]},
+                            "abl":{
+                                "sing":i["key"][:-1] + data["Fem"]["FORM"]["abl"]["sing"], 
+                                "plur":i["key"][:-1] + data["Fem"]["FORM"]["abl"]["plur"]},
+                            "Def":i["senses"],
+                            "weight":7}})
 
 os.remove(filename)
 with open(filename, 'w') as f:
     json.dump(data, f, indent=4)
-
