abb80b416d17d325a0c02f70a202e119532bfd4b
BigB-man <93968558+BigB-man@users.noreply.github.com>
2024-01-30 15:55:25 +1100

    Very sad update
    stopped working for a while as i hit a roadblock doing the other declensions, gave up on the idea of automating using the word lists, so i manually inputted forms of each noun i could find in my grammar book (fulfilling the minimum case of "Range of words within the Cambridge Latin course")

--- json/json_process.py
+++ json/json_process.py
@@ -6,66 +6,56 @@ script_directory = os.path.dirname(os.path.realpath(__file__))
 
 # Set the working directory to the script's directory
 os.chdir(script_directory)
-filename = 'NounDeclension1.json'
-print("Current working directory:", os.getcwd())
-letters =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Z']
-with open(filename, 'r',encoding='utf-8') as k:
-    data = json.load(k)
-index = 0
-for j in letters:
-    os.chdir('files')
-    sourcefile = 'ls_'+letters[index]+'.json'
-    with open(sourcefile, 'r', encoding='utf-8') as f:
-        source = json.load(f)
-        index +=1
-    os.chdir(script_directory)
-    for i in source:#declension 1, i still need to implement to change for masculine words
-        if i["entry_type"] == "main":
-            if i["part_of_speech"] == "noun":
-                if i["declension"] == 1:
-                    if i["gender"] == "F":
-                        data["Fem"].update({i["key"]:{
-                            "nom":{
-                                "sing":i["title_orthography"][:-1] + data["Fem"]["FORM"]["nom"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Fem"]["FORM"]["nom"]["plur"]},
-                            "acc":{
-                                "sing":i["title_orthography"][:-1] + data["Fem"]["FORM"]["acc"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Fem"]["FORM"]["acc"]["plur"]},
-                            "gen":{
-                                "sing":i["title_orthography"][:-1] + data["Fem"]["FORM"]["gen"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Fem"]["FORM"]["gen"]["plur"]},
-                            "dat":{
-                                "sing":i["title_orthography"][:-1] + data["Fem"]["FORM"]["dat"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Fem"]["FORM"]["dat"]["plur"]},
-                            "abl":{
-                                "sing":i["title_orthography"][:-1] + data["Fem"]["FORM"]["abl"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Fem"]["FORM"]["abl"]["plur"]
-                            },
-                            "Def":i["senses"],
-                            "weight":7
-                            }})
-                    elif i["gender"] == "M":
-                        data["Masc"].update({i["key"]:{
-                            "nom":{
-                                "sing":i["title_orthography"][:-1] + data["Masc"]["FORM"]["nom"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Masc"]["FORM"]["nom"]["plur"]},
-                            "acc":{
-                                "sing":i["title_orthography"][:-1] + data["Masc"]["FORM"]["acc"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Masc"]["FORM"]["acc"]["plur"]},
-                            "gen":{
-                                "sing":i["title_orthography"][:-1] + data["Masc"]["FORM"]["gen"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Masc"]["FORM"]["gen"]["plur"]},
-                            "dat":{
-                                "sing":i["title_orthography"][:-1] + data["Masc"]["FORM"]["dat"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Masc"]["FORM"]["dat"]["plur"]},
-                            "abl":{
-                                "sing":i["title_orthography"][:-1] + data["Masc"]["FORM"]["abl"]["sing"], 
-                                "plur":i["title_orthography"][:-1] + data["Masc"]["FORM"]["abl"]["plur"]
-                            },
-                            "Def":i["senses"],
-                            "weight":7
-                            }})
 
-os.remove(filename)
-with open(filename, 'w', encoding='utf-8') as f:
-    json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+caseum = True
+
+while caseum:
+    while caseum:
+        caseum = False
+        try:
+            case = int(input("Declension: "))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+            caseum = True
+    caseum = True
+    
+    filename = 'NounDeclension'+str(case)+'.json'
+    
+    with open(filename, 'r',encoding='utf-8') as k:
+        data = json.load(k)
+    
+    while caseum:
+        caseum = False
+        try:
+            gend = int(input("Gender: F,M,N? "))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+            caseum = True
+    caseum = True
+    genders=["Fem","Masc","Neut"]
+
+    data[genders[gend-1]].update({input("word"):{
+        "nom":{
+            "sing":input("Nom sing: "), 
+            "plur":input("Nom plur: ")},
+        "acc":{
+            "sing":input("Acc sing: "), 
+            "plur":input("Acc plur: ")},
+        "gen":{
+            "sing":input("Gen sing: "), 
+            "plur":input("Gen plur: ")},
+        "dat":{
+            "sing":input("Dat sing: "), 
+            "plur":input("Dat plur: ")},
+        "abl":{
+            "sing":input("abl sing: "), 
+            "plur":input("abl plur: ")
+        },
+        "Def":"idk",
+        "weight":7
+        }})
+    os.remove(filename)
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
---
+++ test.py
@@ -0,0 +1,1 @@
+print("Username is: " + input("Enter username:"))
